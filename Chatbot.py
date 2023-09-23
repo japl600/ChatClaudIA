9ada45b87cdabaa7583965953d40540424c8b40f
Joel Prada <aprada@pinterest.com>
2023-09-23 04:03:08 -0500

    Updating bg

--- Chatbot.py
+++ Chatbot.py
@@ -1,4 +1,5 @@
 import openai
+import base64
 import streamlit as st
 
 with st.sidebar:
@@ -10,16 +11,24 @@ with st.sidebar:
 st.title("💬 Claud-IA")
 st.caption("💅 A Belcorp chatbot powered by OpenAI LLM")
 
-page_bg_img = '''
-<style>
-.stApp {
-background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
-background-size: cover;
-}
-</style>
-'''
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
 
-st.markdown(page_bg_img, unsafe_allow_html=True)
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+set_background('./images/background.png')
 
 
 if "messages" not in st.session_state:
